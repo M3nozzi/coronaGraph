382be58e4681fc1796e566db44e2688a525a4dab
Fabio Menozzi <f.menozzi@yahoo.com>
2020-08-05 11:26:24 -0300

    api done

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { Component } from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
+import { fetchData } from './api';
 
 class App extends Component {
+
+    async componentDidMount() {
+        const data = await fetchData();
+        console.log(data)
+    }
+
     render() {
         return (
             <div className={styles.container}>
