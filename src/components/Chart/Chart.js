f5b5b2abcf3f6592c0deb29ec50ad0405f403b12
Fabio Menozzi <f.menozzi@yahoo.com>
2020-08-05 18:49:47 -0300

    building the graph

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,6 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
+import styles from './Chart.module.css';
 
 const Chart = () => {
+    const [ dailyData, setDailyData ] = useState({});
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        }
+
+        fetchAPI();
+    });
+
+    const lineChart = (
+        dailyData[0]
+       ?  (< Line
+            data={{
+              labels: dailyData(({ date }) => date),
+              datasets: [{
+                data: dailyData(({ confirmed }) => confirmed),
+                label: 'Infected',
+                borderColor: '#3333ff',
+                fill: true,
+              }, {
+                data: dailyData(({ deaths }) => deaths),
+                label: 'Deaths',
+                borderColor: '#FA2C0A',
+                fill: true,
+              }],
+            }}
+        />) : null
+    );
+
     return (
         <h1>Chart</h1>
     )
